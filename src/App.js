13438abac50cc552402fbca527024bf09c9fd716
adoherty8 <azdoherty91@gmail.com>
2019-10-08 19:32:36 -0400

    handling some state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        Some app
+        <div className="test">
+          more app
+        </div>
+        <AlertButton></AlertButton>
+      </div>
+    );}
+  }
+
+var myalert = function(stuff){
+  alert("test " + stuff)
+}
+
+class AlertButton extends React.Component{
+  constructor(props){
+    super(props);
+    this.buttonref = React.createRef();
+    this.state = {clicks: 0}
+
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick(){
+    this.setState((state, props) =>{
+      return {clicks: state.clicks + 1}
+    });
+    alert("clicks: " + this.state.clicks)
+
+  }
+
+  render(){
+    return (
+      <Button className="Primary"
+      onClick={this.handleClick}>
+        Some button
+      </Button>
+    );
+  }
+  
 }
 
 export default App;
